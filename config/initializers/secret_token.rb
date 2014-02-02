b40aff52c4001854b3770dc40b3bb2498060ac4c
fernvilla <fvilla18@gmail.com>
2014-02-01 23:26:49 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GymTime::Application.config.secret_key_base = 'f62c8dd60478ef5658d9fc5371ca75cc5f9c099ad1c1c7a7ce69aed49f0daf6036ee00b7e7e8a40a645a29b970545622b4cfc1e550b200ef620097546cd96a8a'

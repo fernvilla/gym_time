790141a4eb76d2f92b7b34441760a3c253dc0a22
fernvilla <fvilla18@gmail.com>
2014-02-05 14:47:12 -0800

    Generate workout controller and views

---
+++ app/controllers/workouts_controller.rb
@@ -0,0 +1,53 @@
+class WorkoutsController < ApplicationController
+  def index
+  	@workouts = Workout.all
+  end
+
+  def new
+  	@workout = Workout.new
+  end
+
+  def show
+  	@workout = Workout.find(params[:id])
+  end
+
+  def edit
+  	@workout = Workout.find(params[:id])
+  end
+
+  def create
+  	@workout = Workout.new(workout_params)
+
+  	if @workout.save
+  		flash[:notice] = "Successfully Created A Workout!"
+  		redirect_to action: 'index'
+  	else
+  		render action: 'new'
+  	end
+  end
+
+  def update
+  	@workout = Workout.find(params[:id])
+
+  	if @workout.update(workout_params)
+  		flash[:notice] = "Successfully Updated Your Workout!"
+  		redirect_to action: 'index'
+  	else
+  		render action: 'edit'
+		end
+	end
+
+	def destroy
+		@workout = Workout.find(params[:id])
+		@workout.destroy
+		flash[:notice] = "Successfully Deleted Your Workout!"
+		redirect_to action: 'index'
+	end
+
+	private
+
+	def workout_params
+		params.require(:workout).permit(:bodypart, :exercise, :workout_date)	
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 GymTime::Application.routes.draw do
   get "home/index"
+
+  resources :workouts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

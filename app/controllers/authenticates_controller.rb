84811201c1d8ca7bc51bc98dbf93d5ff4c64fe67
fernvilla <fvilla18@gmail.com>
2014-02-13 17:24:35 -0800

    Fix login issue

--- app/controllers/authenticates_controller.rb
+++ app/controllers/authenticates_controller.rb
@@ -11,7 +11,7 @@ class AuthenticatesController < ApplicationController
 	def create
 		user = User.find_by(username: params[:user][:username])
 		# Mongoid.yml file must be set to 'raise_not_found_error: false' to return nil if no user found
-		if (user != nil) && (user.authenticated?(params[:user][:password]))
+		if user && user.authenticated?(params[:user][:password])
 			session[:user_id] = user.id
 			redirect_to workouts_path
 		else
@@ -24,10 +24,10 @@ class AuthenticatesController < ApplicationController
 		if current_user
 			session[:user_id] = nil
 			redirect_to new_authenticate_path
-			flash[:notice] = " You have successfully logged off"
+			flash[:notice] = "You have successfully logged off"
 		else
 			redirect_to new_authenticate_path
-			flash[:notice] = " You are not currently logged in"
+			flash[:notice] = "You are not currently logged in"
 		end
 	end
 end
